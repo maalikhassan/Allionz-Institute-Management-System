1b1b5f3b461b4ecb11d96a820328fdfcef8dd20a
Pasindu Madhuwantha <pasinduogdev@gmail.com>
2024-11-20 15:37:43 +0530

    modified test

--- src/test/test.java
+++ src/test/test.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package test;
 
-/**
- *
- * @author pasin
- */
 public class test {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
