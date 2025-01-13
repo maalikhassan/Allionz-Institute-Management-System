d7e42894cf10c9d4a2f2f25608c95f1bf1c714a2
Maalik_Hassan <maalikhassan1@gmail.com>
2025-01-13 14:32:50 +0530

    added logger class and initialized, added backup with error to test
    backup is with an error, dont worry about that. just press button and see if log file gets created with that error shown

---
+++ src/model/LogManager.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.logging.*;
+
+
+public class LogManager {
+
+    public static Logger logger;
+    public static FileHandler handler;
+
+    static {
+        try {
+            logger = Logger.getLogger("Log1");
+            handler = new FileHandler("Allionz-IMS.txt", true);
+            handler.setFormatter(new SimpleFormatter());
+            logger.addHandler(handler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+}
